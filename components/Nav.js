753c3afa1abfab7b397987d8bbe679132d1f8956
hamza-elsawy97 <153780196+hamza-elsawy97@users.noreply.github.com>
2025-02-18 08:21:36 +0300

    nav

--- components/Nav.js
+++ components/Nav.js
@@ -6,7 +6,7 @@ import {
   HiRectangleGroup,
   HiChatBubbleBottomCenterText,
   HiEnvelope,
-} from 'react-icons/hi2';
+} from 'react-icons/hi2'
 
 // nav data
 export const navData = [
@@ -24,10 +24,39 @@ export const navData = [
     path: '/contact',
     icon: <HiEnvelope />,
   },
-];
+]
+
+// next link
+import Link from 'next/link'
+
+//next router
+import { useRouter } from 'next/router'
 
 const Nav = () => {
-  return <nav>nav</nav>;
-};
+  const router = useRouter()
+  const pathname = router.pathname
 
-export default Nav;
+  return (
+    <nav
+      className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max
+    bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen"
+    >
+      {/*inner */}
+      <div
+        className="flex w-full xl:flex-col items-center justify-between
+      xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 bg-white/10
+      backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full"
+      >
+        {navData.map((link, index) => {
+          return (
+            <Link href={link.path} key={index }>
+              <div>{link.icon}</div>
+            </Link>
+          )
+        })}
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+//components
+import Layout from '../components/Layout';
 
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
 export default MyApp;
